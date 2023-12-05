14751860abd61ef70def20b0bbe3f566b5fb497e
Carlos Ochoa <uligibson@hotmail.com>
2023-12-04 23:00:01 -0600

    WIP: challenge #1

--- retos/reto-1/main.ts
+++ retos/reto-1/main.ts
@@ -1,1 +1,8 @@
-// Aun no disponible
+export function findFirstRepeated(gifts: number[]): number {
+  // TODO: Corregir este código para que pase todos los tests
+  const uniqueGifts = new Set(gifts);
+  if (uniqueGifts.size === gifts.length) {
+    return -1;
+  }
+  return 0;
+}
