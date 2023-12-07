33a2fdfb43e5bef906ea4420e844c6e2a9a0918c
Carlos Ochoa <uligibson@hotmail.com>
2023-12-07 12:28:09 -0600

    challenge #1 solution added

--- retos/reto-1/main.ts
+++ retos/reto-1/main.ts
@@ -1,8 +1,25 @@
 export function findFirstRepeated(gifts: number[]): number {
-  // TODO: Corregir este código para que pase todos los tests
-  const uniqueGifts = new Set(gifts);
-  if (uniqueGifts.size === gifts.length) {
-    return -1;
-  }
-  return 0;
+  // Objeto para rastrear la aparición de cada número de regalo.
+  const giftAppearanceTracker: Record<number, boolean> = {}
+  // Variable para almacenar el primer número de regalo repetido encontrado.
+  let firstDuplicate: number = -1
+
+  // Recorremos cada número de regalo en la lista.
+  gifts.find((giftNumber: number) => {
+    // Verificamos si el número actual ya ha aparecido.
+    if (giftAppearanceTracker[giftNumber]) {
+      // Si ya ha aparecido, establecemos el primer duplicado con el número actual
+      // y retornamos true para detener la búsqueda.
+      firstDuplicate = giftNumber
+      return true
+    }
+    // Marcamos el número de regalo como visto, estableciendo su valor en true.
+    giftAppearanceTracker[giftNumber] = true
+    // Retornamos false para continuar la búsqueda.
+    // (El método `find` ignora este false y solo detiene la búsqueda cuando retorna true).
+    return false
+  })
+
+  // Retornamos el primer duplicado encontrado o -1 si no se encontró ninguno.
+  return firstDuplicate
 }
