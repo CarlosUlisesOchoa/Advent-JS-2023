d73c7ef1444683e025f13e2b3f940b15642035ea
Carlos Ochoa <uligibson@hotmail.com>
2023-12-07 12:27:26 -0600

    jest config updated

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,20 +1,20 @@
 // jest.config.mjs
 export default {
-  preset: "ts-jest/presets/default-esm", // use the ESM preset
+  preset: 'ts-jest/presets/default-esm', // use the ESM preset
   globals: {
-    "ts-jest": {
+    'ts-jest': {
       useESM: true,
     },
   },
   moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1", // map .js files to their source .ts files
+    '^(\\.{1,2}/.*)\\.js$': '$1', // map .js files to their source .ts files
   },
   transform: {
-    "^.+\\.ts$": "ts-jest",
+    '^.+\\.ts$': 'ts-jest',
   },
   testMatch: [
-    "<rootDir>/retos/dist/**/*.test.js", // This pattern should match your test files
+    '<rootDir>/dist/**/*.test.js', // This pattern should match your test files
   ],
-  testEnvironment: "node",
+  testEnvironment: 'node',
   // ... any other config you might have ...
-};
+}
