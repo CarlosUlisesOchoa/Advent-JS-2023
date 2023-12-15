2a43fa5d20763278fce5345d528ad7ca9db0f0be
Carlos Ochoa <uligibson@hotmail.com>
2023-12-14 18:04:01 -0600

    challenge #7 main.ts refactored

--- retos/reto-7/main.ts
+++ retos/reto-7/main.ts
@@ -1,76 +1,55 @@
 export function drawGift(size: number, symbol: string): string {
   //
-  // Primero sería interesante validar los parametros recibidos (aunque el reto no lo pide y si lo descomentas te restará puntos OJO)
-  // if (size < 1 || symbol.length !== 1) return '\n'
-
+  // Definimos algunas constantes para controlar las iteraciones y la longitud de las líneas
   const maxIteractions = size * 2
-  const maxLengthPerLine = size * 2 - 1
-  const maxLinesAsResult = size * 2 - 1
+  const maxLengthPerLine = maxIteractions - 1
+  const maxLinesAsResult = maxIteractions - 1
   const linesArray: string[] = []
+
+  // Bucle principal en donde con cada iteración se construirá cada línea del regalo 3D
   for (let i = 1; i < maxIteractions; i++) {
     let currentLine = ''
+
+    // Construcción de la primera mitad del regalo
     if (i <= size) {
+      // Esto agrega los espacios iniciales para la inclinación 3D
       currentLine += ' '.repeat(size - i)
+
+      // Primera línea: solo borde superior del regalo
       if (i === 1) {
-        currentLine += '#'.repeat(size - 1)
-      } else if (i === size) {
+        currentLine += '#'.repeat(size - 1) // i = 1
+      }
+      // Línea central: toda la anchura del regalo
+      else if (i === size) {
         currentLine += '#'.repeat(size)
         currentLine += symbol.repeat(size - 2)
-      } else if (i >= 2 && i < size) {
+      }
+      // Líneas intermedias: combinación de borde y símbolo
+      else if (i >= 2 && i < size) {
         currentLine += '#'
-        let tempCounter = 0
+        let symbolInSerieCounter = 0
         while (currentLine.length < maxLengthPerLine - 1) {
           currentLine += symbol
-          tempCounter++
-          if (tempCounter % (size - 2) === 0) {
-            // Recordemos que como máximo puede haber (size - 2) simbolos en fila. Cuando esto sucede deben separarse con '#'
+          symbolInSerieCounter++
+          // Insertamos un '#' después de cada (size - 2) símbolos
+          if (symbolInSerieCounter === size - 2) {
             currentLine += '#'
+            symbolInSerieCounter = 0
           }
         }
       }
+      // Añadimos borde derecho y salto de línea
       currentLine += '#\n'
-    } else {
+    }
+    // Construcción de la segunda mitad del regalo
+    else {
+      // Reutilizando líneas previas pero no sin antes quitarles los espacios iniciales
       const linesLeftBeforeFinish = maxLinesAsResult - linesArray.length
       currentLine = linesArray[linesLeftBeforeFinish - 1].trimStart()
     }
+    // Añadimos la línea construida al array
     linesArray.push(currentLine)
   }
+  // Unimos todas las líneas en una sola cadena y la devolvemos
   return linesArray.join('')
 }
-
-const asd1 = drawGift(4, '+')
-
-/*
-   ####
-  #++##
- #++#+#
-####++#
-#++#+#
-#++##
-####
-*/
-
-// ////////////////////////////////////////////////////////////////////////////////
-
-const asd2 = drawGift(5, 'x')
-
-/*
-    #####
-   #***##
-  #***#*#
- #***#**#
-#####***#
-#***#**#
-#***#*#
-#***##
-#####
-*/
-
-// ////////////////////////////////////////////////////////////////////////////////
-
-const asd3 = drawGift(1, '^')
-/*
-#
-*/
-
-console.log() // tetemp
