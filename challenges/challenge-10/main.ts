fdfe74a651c9547a1a7aad6c5b6f582abd561b6f
Carlos Ochoa <uligibson@hotmail.com>
2023-12-19 21:38:15 -0600

    solution to challenge #10 added

--- challenges/challenge-10/main.ts
+++ challenges/challenge-10/main.ts
@@ -1,3 +1,35 @@
 export function createChristmasTree(ornaments: string, height: number): string {
-  return '|'
+  if (ornaments.length === 0 || height === 0) return '|\n'
+
+  let result: string[] = []
+  let currentIndex = 0
+
+  for (let i = 0; i < height; i++) {
+    let line = ' '.repeat(height - i - 1)
+    // Se agregan los espacios correspondientes a la linea
+    for (let j = 0; j < i * 2 + 1; j++) {
+      // Número correcto de adornos
+      if (j % 2 === 0) {
+        line += ornaments[currentIndex]
+        // Se agrega el adorno
+        currentIndex = (currentIndex + 1) % ornaments.length
+        // Se actualiza currentIndex para usar el adorno siguiente
+      } else {
+        line += ' '
+        // Cuando no se agrega adorno, se agrega un espacio
+      }
+    }
+    result.push(line)
+    // Se agrega la linea recien creada al Array
+  }
+
+  result.push(' '.repeat(height - 1) + '|')
+  // Se agrega el tronco del árbol
+  // El tronco se posiciona al centro, para lograr esto:
+  // Se agregan (height - 1) espacios y luego '|'
+
+  return result.join('\n') + '\n'
 }
+//
+// Esta solución da 240 puntos
+// NOTA: Se recomienda eliminar los comentarios antes de subir las soluciones
