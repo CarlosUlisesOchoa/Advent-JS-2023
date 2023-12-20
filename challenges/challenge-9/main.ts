fe0ce0a10e8a4688f1bee2f4f1aa2b267c20ec0d
Carlos Ochoa <uligibson@hotmail.com>
2023-12-19 19:33:36 -0600

    solution to challenge #9 added

--- challenges/challenge-9/main.ts
+++ challenges/challenge-9/main.ts
@@ -1,4 +1,26 @@
 export function adjustLights(lights: string[]): number {
-  // Code here
-  return 0
+  //
+  function calculatePattern(lights: string[], firstColor: string, secondColor: string): number {
+    let count = 0
+    // Contador para el número de cambios necesarios
+    let expectedColor = firstColor
+    // El color esperado en la posición actual
+
+    for (const light of lights) {
+      if (light !== expectedColor) {
+        // Incrementa el contador si el color actual no es el esperado
+        count++
+      }
+      // Alterna el color esperado para la siguiente luz
+      expectedColor = expectedColor === firstColor ? secondColor : firstColor
+    }
+    return count
+  }
+  //
+  // Calcula el número de cambios requeridos para cada patrón
+  const pattern1 = calculatePattern(lights, '🔴', '🟢')
+  const pattern2 = calculatePattern(lights, '🟢', '🔴')
+
+  // Devuelve el menor número de cambios entre los dos patrones
+  return Math.min(pattern1, pattern2)
 }
